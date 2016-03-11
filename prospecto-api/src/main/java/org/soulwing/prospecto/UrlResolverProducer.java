bf63dd36bdbe8517908dd9c46abbe4385bf408cd
Carl Harris <ceharris@kauai-sb.local>
2016-03-11 11:06:42 -0500

    added UrlResolver API and provider SPI

---
+++ prospecto-api/src/main/java/org/soulwing/prospecto/UrlResolverProducer.java
@@ -0,0 +1,65 @@
+/*
+ * File created on Mar 11, 2016
+ *
+ * Copyright (c) 2016 Carl Harris, Jr
+ * and others as noted
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.soulwing.prospecto;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+import org.soulwing.prospecto.api.UrlResolver;
+import org.soulwing.prospecto.api.UrlResolver;
+import org.soulwing.prospecto.spi.UrlResolverProvider;
+
+/**
+ * An object that produces {@link UrlResolver} instances.
+ *
+ * @author Carl Harris
+ */
+public class UrlResolverProducer {
+
+  private static Singleton<UrlResolverProducer> singleton =
+      new Singleton<UrlResolverProducer>() {
+        @Override
+        protected UrlResolverProducer newInstance() {
+          final ServiceLoader<UrlResolverProvider> providers =
+              ServiceLoader.load(UrlResolverProvider.class);
+          final Iterator<UrlResolverProvider> i = providers.iterator();
+          if (!i.hasNext()) {
+            throw new NoSuchProviderException(
+                UrlResolverProvider.class.getSimpleName());
+          }
+
+          return new UrlResolverProducer(i.next());
+        }
+      };
+
+  private final UrlResolverProvider provider;
+
+  private UrlResolverProducer(UrlResolverProvider provider) {
+    this.provider = provider;
+  }
+
+  /**
+   * Creates a new URL resolver.
+   * @return URL resolver
+   */
+  public static UrlResolver newResolver() {
+    return singleton.getInstance().provider.newResolver();
+  }
+  
+}
---
+++ prospecto-api/src/main/java/org/soulwing/prospecto/api/UrlResolver.java
@@ -0,0 +1,42 @@
+/*
+ * File created on Mar 11, 2016
+ *
+ * Copyright (c) 2016 Carl Harris, Jr
+ * and others as noted
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.soulwing.prospecto.api;
+
+/**
+ * A service that resolvers the actual URL string for a value node of type
+ * <em>url</em>.
+ * <p>
+ * An implementation <em>must</em> be thread safe as it may be used to resolve
+ * URLs for multiple concurrent view generation requests.
+ *
+ * @author Carl Harris
+ */
+public interface UrlResolver {
+
+  /**
+   * Resolves a URL string for a view node of type <em>url</em>.
+   * @param node the subject URL view node
+   * @param context view context
+   * @return URL string
+   * @throws UnresolvedUrlException if the resolver cannot resolve the URL
+   *    string for the subject view node
+   */
+  String resolve(ViewNode node, ViewContext context);
+
+}
