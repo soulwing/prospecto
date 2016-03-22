2edc2f6beac701b8d8fc980ec0483e49e0ed6f76
Carl Harris <ceharris@kauai-sb.local>
2016-03-22 13:35:03 -0400

    added type coercion support

--- prospecto-runtime/src/main/java/org/soulwing/prospecto/runtime/converter/ConverterSupport.java
+++ prospecto-runtime/src/main/java/org/soulwing/prospecto/runtime/converter/ConverterSupport.java
@@ -18,6 +18,14 @@
  */
 package org.soulwing.prospecto.runtime.converter;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Date;
+
+import org.soulwing.prospecto.api.Scope;
 import org.soulwing.prospecto.api.ViewContext;
 import org.soulwing.prospecto.api.ViewNode;
 import org.soulwing.prospecto.api.converter.ValueTypeConverter;
@@ -53,6 +61,128 @@ public class ConverterSupport {
     }
 
     return model;
+  }
+
+  /**
+   * Converts a view value to a model value for a node.
+   * @param type target model type
+   * @param value view value
+   * @param node the subject node
+   * @param context view context
+   * @return model value
+   * @throws Exception
+   */
+  public static Object toModelValue(Class<?> type, Object value, Scope node,
+      ViewContext context) throws Exception {
+    if (value == null) return null;
+
+    final ValueTypeConverter<?> localConverter = node.get(ValueTypeConverter.class);
+    if (localConverter != null) {
+      return localConverter.toObject(coerce(value, localConverter.getViewType()));
+    }
+
+    for (ValueTypeConverter<?> converter : context.getValueTypeConverters()) {
+      if (converter.supports(value.getClass())) {
+        return converter.toObject(coerce(value, converter.getViewType()));
+      }
+    }
+
+    return coerce(value, type);
+  }
+
+  /**
+   * Coerces a view value to a model value type.
+   * @param value view value
+   * @param type model value type
+   * @return coerced value
+   * @throws IllegalArgumentException if the value cannot be coerced
+   */
+  private static Object coerce(Object value, Class<?> type) throws Exception {
+    assert value != null;
+    if (type.isInstance(value)) return value;
+    if (Enum.class.isAssignableFrom(type) && value instanceof String) {
+      final Method method = type.getMethod("valueOf", String.class);
+      return method.invoke(type, value);
+    }
+    if (Date.class.isAssignableFrom(type) && value instanceof Number) {
+      final Constructor<Date> constructor = Date.class.getConstructor(long.class);
+      return constructor.newInstance(((Number) value).longValue());
+    }
+    if ((Byte.class.isAssignableFrom(type) || byte.class.isAssignableFrom(type))
+        && value instanceof Number) {
+      return ((Number) value).byteValue();
+    }
+    if ((Short.class.isAssignableFrom(type) || short.class.isAssignableFrom(type))
+        && value instanceof Number) {
+      return ((Number) value).shortValue();
+    }
+    if ((Integer.class.isAssignableFrom(type) || int.class.isAssignableFrom(type))
+        && value instanceof Number) {
+      return ((Number) value).intValue();
+    }
+    if ((Long.class.isAssignableFrom(type) || long.class.isAssignableFrom(type))
+        && value instanceof Number) {
+      return ((Number) value).longValue();
+    }
+    if (BigInteger.class.isAssignableFrom(type) && value instanceof Number) {
+      return BigInteger.valueOf(((Number) value).longValue());
+    }
+    if ((Float.class.isAssignableFrom(type) || float.class.isAssignableFrom(type))
+        && value instanceof Number) {
+      return ((Number) value).floatValue();
+    }
+    if ((Double.class.isAssignableFrom(type) || double.class.isAssignableFrom(type))
+        && value instanceof Number) {
+      return ((Number) value).doubleValue();
+    }
+    if (BigDecimal.class.isAssignableFrom(type) && value instanceof Number) {
+      return new BigDecimal(((Number) value).doubleValue());
+    }
+
+    Object result = coerceUsingValueOf(type, value);
+    if (result != null) return result;
+
+    result = coerceUsingConstructor(type, value);
+    if (result != null) return result;
+
+    throw new IllegalArgumentException("cannot coerce value of type "
+        + value.getClass().getName() + " to type " + type.getName());
+  }
+
+  private static Object coerceUsingConstructor(Class<?> type, Object value) {
+    Class<?> valueType = value.getClass();
+    while (valueType != null) {
+      try {
+        final Constructor constructor = type.getConstructor(valueType);
+        return constructor.newInstance(value);
+      }
+      catch (InstantiationException
+          | NoSuchMethodException
+          | InvocationTargetException
+          | IllegalAccessException ex) {
+        valueType = valueType.getSuperclass();
+      }
+    }
+    return null;
+  }
+
+  private static Object coerceUsingValueOf(Class<?> type, Object value) {
+    Class<?> valueType = value.getClass();
+    while (valueType != null) {
+      try {
+        final Method valueOfMethod = type.getMethod("valueOf", valueType);
+        if (type.isAssignableFrom(valueOfMethod.getReturnType())) {
+          return valueOfMethod.invoke(type, value);
+        }
+      }
+      catch (IllegalAccessException
+          | InvocationTargetException
+          | NoSuchMethodException ex) {
+        assert true;
+      }
+      valueType = valueType.getSuperclass();
+    }
+    return null;
   }
 
 }
--- prospecto-runtime/src/test/java/org/soulwing/prospecto/runtime/converter/ConverterSupportTest.java
+++ prospecto-runtime/src/test/java/org/soulwing/prospecto/runtime/converter/ConverterSupportTest.java
@@ -20,9 +20,14 @@ package org.soulwing.prospecto.runtime.converter;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Collections;
+import java.util.Date;
+import java.util.Objects;
 
 import org.jmock.Expectations;
 import org.jmock.auto.Mock;
@@ -88,6 +93,178 @@ public class ConverterSupportTest {
 
     assertThat(ConverterSupport.toViewValue(MODEL_VALUE, node, viewContext),
         is(equalTo(VIEW_VALUE)));
+  }
+
+  @Test
+  public void testCoerceToSameType() throws Exception {
+    final Object value = new Object();
+    coerceAndValidate(Object.class, value, value);
+  }
+
+  @Test
+  public void testCoerceStringToEnum() throws Exception {
+    coerceAndValidate(MockEnum.class, MockEnum.VALUE.name(), MockEnum.VALUE);
+  }
+
+  @Test
+  public void testCoerceNumberToDate() throws Exception {
+    final Date date = new Date();
+    coerceAndValidate(Date.class, date.getTime(), date);
+  }
+
+  @Test
+  public void testCoerceNumberToByte() throws Exception {
+    coerceAndValidate(Byte.class, -1L, (byte) -1);
+  }
+
+  @Test
+  public void testCoerceNumberToBytePrimitive() throws Exception {
+    coerceAndValidate(byte.class, -1L, (byte) -1);
+  }
+
+  @Test
+  public void testCoerceNumberToShort() throws Exception {
+    coerceAndValidate(Short.class, -1L, (short) -1);
+  }
+
+  @Test
+  public void testCoerceNumberToShortPrimitive() throws Exception {
+    coerceAndValidate(short.class, -1L, (short) -1);
+  }
+
+  @Test
+  public void testCoerceNumberToInteger() throws Exception {
+    coerceAndValidate(Integer.class, -1L, -1);
+  }
+
+  @Test
+  public void testCoerceNumberToIntegerPrimitive() throws Exception {
+    coerceAndValidate(int.class, -1L, -1);
+  }
+
+  @Test
+  public void testCoerceNumberToLong() throws Exception {
+    coerceAndValidate(Long.class, -1L, -1L);
+  }
+
+  @Test
+  public void testCoerceNumberToLongPrimitive() throws Exception {
+    coerceAndValidate(long.class, -1L, -1L);
+  }
+
+  @Test
+  public void testCoerceNumberToFloat() throws Exception {
+    coerceAndValidate(Float.class, -1L, -1.0f);
+  }
+
+  @Test
+  public void testCoerceNumberToFloatPrimitive() throws Exception {
+    coerceAndValidate(float.class, -1L, -1.0f);
+  }
+
+  @Test
+  public void testCoerceNumberToDouble() throws Exception {
+    coerceAndValidate(Double.class, -1L, -1.0);
+  }
+
+  @Test
+  public void testCoerceNumberToDoublePrimitive() throws Exception {
+    coerceAndValidate(double.class, -1L, -1.0);
+  }
+
+  @Test
+  public void testCoerceNumberToBigInteger() throws Exception {
+    coerceAndValidate(BigInteger.class, -1L, BigInteger.ONE.negate());
+  }
+
+  @Test
+  public void testCoerceNumberToBigDecimal() throws Exception {
+    coerceAndValidate(BigDecimal.class, -1L, BigDecimal.ONE.negate());
+  }
+
+  @Test
+  public void testCoerceObjectToValueTypeUsingValueOf() throws Exception {
+    final MockValueTypeWithValueOf value =
+        new MockValueTypeWithValueOf("value");
+    coerceAndValidate(MockValueTypeWithValueOf.class, "value", value);
+  }
+
+  @Test
+  public void testCoerceObjectToValueTypeUsingConstructor() throws Exception {
+    final MockValueTypeWithValueOf value =
+        new MockValueTypeWithValueOf("value");
+    coerceAndValidate(MockValueTypeWithValueOf.class, "value", value);
+  }
+
+  @SuppressWarnings("unchecked")
+  private <T> void coerceAndValidate(Class<T> type, Object value, T expected)
+      throws Exception {
+    context.checking(converterExpectations());
+    final Object actual = ConverterSupport.toModelValue(type, value, node,
+        viewContext);
+    assertThat(actual, is(instanceOf(type)));
+    assertThat((T) actual, is(equalTo(expected)));
+  }
+
+  private Expectations converterExpectations() {
+    return new Expectations() {
+      {
+        oneOf(node).get(ValueTypeConverter.class);
+        will(returnValue(null));
+        oneOf(viewContext).getValueTypeConverters();
+        will(returnValue(Collections.emptyList()));
+      }
+    };
+  }
+
+  enum MockEnum {
+    VALUE
+  }
+
+  public static class MockValueTypeWithConstructor {
+    private final Object value;
+
+    public MockValueTypeWithConstructor(Object value) {
+      this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj == this) return true;
+      if (!(obj instanceof MockValueTypeWithConstructor)) return false;
+      return Objects.equals(this.value,
+          ((MockValueTypeWithConstructor) obj).value);
+    }
+  }
+
+  public static class MockValueTypeWithValueOf {
+    private final Object value;
+
+    public static MockValueTypeWithValueOf valueOf(Object value) {
+      return new MockValueTypeWithValueOf(value);
+    }
+
+    private MockValueTypeWithValueOf(Object value) {
+      this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj == this) return true;
+      if (!(obj instanceof MockValueTypeWithValueOf)) return false;
+      return Objects.equals(this.value,
+          ((MockValueTypeWithValueOf) obj).value);
+    }
   }
 
 }
