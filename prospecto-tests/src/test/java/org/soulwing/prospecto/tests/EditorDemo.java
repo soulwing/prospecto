d53ef3da485eec02010b9dac5e4c90b8abbe42ed
Carl Harris <ceharris@kauai-sb.local>
2016-03-27 06:22:09 -0400

    added simple editor demo

--- prospecto-tests/src/test/java/org/soulwing/prospecto/tests/EditorDemo.java
+++ prospecto-tests/src/test/java/org/soulwing/prospecto/tests/EditorDemo.java
@@ -22,26 +22,34 @@ import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.Date;
 
+import org.soulwing.prospecto.ViewContextProducer;
 import org.soulwing.prospecto.ViewReaderFactoryProducer;
 import org.soulwing.prospecto.ViewTemplateBuilderProducer;
 import org.soulwing.prospecto.ViewWriterFactoryProducer;
+import org.soulwing.prospecto.api.AccessMode;
 import org.soulwing.prospecto.api.ModelEditor;
 import org.soulwing.prospecto.api.View;
+import org.soulwing.prospecto.api.ViewContext;
+import org.soulwing.prospecto.api.ViewEntity;
 import org.soulwing.prospecto.api.ViewReader;
 import org.soulwing.prospecto.api.ViewReaderFactory;
 import org.soulwing.prospecto.api.ViewTemplate;
 import org.soulwing.prospecto.api.ViewWriter;
 import org.soulwing.prospecto.api.ViewWriterFactory;
 import org.soulwing.prospecto.api.converter.DateTypeConverter;
-import org.soulwing.prospecto.api.converter.PropertyExtractingValueTypeConverter;
 import org.soulwing.prospecto.api.discriminator.SimpleClassNameDiscriminatorStrategy;
+import org.soulwing.prospecto.api.handler.ViewNodeEntityListener;
+import org.soulwing.prospecto.api.handler.ViewNodeEvent;
+import org.soulwing.prospecto.api.handler.ViewNodeListener;
+import org.soulwing.prospecto.api.handler.ViewNodePropertyEvent;
+import org.soulwing.prospecto.api.handler.ViewNodePropertyListener;
+import org.soulwing.prospecto.api.reference.ReferenceResolver;
 import org.soulwing.prospecto.demo.DepartmentPurchaser;
 import org.soulwing.prospecto.demo.PersonPurchaser;
 import org.soulwing.prospecto.demo.PurchaseItem;
 import org.soulwing.prospecto.demo.PurchaseOrder;
 import org.soulwing.prospecto.demo.Purchaser;
 import org.soulwing.prospecto.demo.Vendor;
-import org.soulwing.prospecto.runtime.context.ConcreteViewContext;
 
 /**
  * TODO: DESCRIBE THE TYPE HERE
@@ -50,7 +58,7 @@ import org.soulwing.prospecto.runtime.context.ConcreteViewContext;
 public class EditorDemo {
 
   public static final ViewTemplate PO_TEMPLATE = ViewTemplateBuilderProducer
-      .object(PurchaseOrder.class)
+      .object("order", PurchaseOrder.class)
         .value("id")
         .value("fob")
         .value("creationDate")
@@ -59,10 +67,11 @@ public class EditorDemo {
               "format", DateTypeConverter.Format.ISO8601_DATE)
         .value("total")
           .source("itemTotal")
-        .value("vendor")
-          .converter(PropertyExtractingValueTypeConverter.class,
-              "modelType", Vendor.class,
-              "propertyName", "name")
+          .allow(AccessMode.READ)
+        .reference("vendor", Vendor.class)
+          .value("id")
+          .value("name")
+          .end()
         .object("purchaser", Purchaser.class)
           .discriminator(SimpleClassNameDiscriminatorStrategy.class,
               "suffix", "Purchaser",
@@ -127,15 +136,57 @@ public class EditorDemo {
 
     View view = reader.readView();
 
+    ViewContext context = ViewContextProducer.newContext();
+
+    context.getListeners().append(new ViewNodePropertyListener() {
+      @Override
+      public void propertyVisited(ViewNodePropertyEvent event) {
+        System.out.format("will inject '%s' at %s\n",
+            event.getValue(), event.getContext().currentViewPathAsString());
+      }
+    });
+
+    context.getListeners().append(new ViewNodeListener() {
+      @Override
+      public void nodeVisited(ViewNodeEvent event) {
+        System.out.format("%s visited\n",
+            event.getContext().currentViewPathAsString());
+      }
+    });
+
+    context.getListeners().append(new ViewNodeEntityListener() {
+      @Override
+      public void entityCreated(ViewNodePropertyEvent event) {
+        System.out.format("created entity: %s\n", event.getValue());
+      }
+
+      @Override
+      public void entityDiscarded(ViewNodePropertyEvent event) {
+        System.out.format("discarded entity: %s\n", event.getValue());
+      }
+    });
+
+    context.getReferenceResolvers().append(new ReferenceResolver() {
+      @Override
+      public boolean supports(Class<?> type) {
+        return Vendor.class.isAssignableFrom(type);
+      }
+
+      @Override
+      public Object resolve(Class<?> type, ViewEntity reference) {
+        return vendor;
+      }
+    });
+
     ModelEditor editor =
-        PO_TEMPLATE.generateEditor(view, new ConcreteViewContext());
+        PO_TEMPLATE.generateEditor(view, context);
 
     editor.update(order);
 
     ViewWriterFactory writerFactory = ViewWriterFactoryProducer
         .getFactory("JSON");
 
-    View updatedView = PO_TEMPLATE.generateView(order, new ConcreteViewContext());
+    View updatedView = PO_TEMPLATE.generateView(order, context);
 
     ViewWriter writer = writerFactory.newWriter(updatedView, System.out);
     writer.writeView();
