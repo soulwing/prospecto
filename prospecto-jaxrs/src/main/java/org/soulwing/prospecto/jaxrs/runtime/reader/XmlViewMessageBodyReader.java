7bb2b5a090b18033f13e561e8b8047d2065bd572
Carl Harris <ceharris@kauai-sb.local>
2016-03-27 06:20:11 -0400

    added message body readers for JSON and XML

---
+++ prospecto-jaxrs/src/main/java/org/soulwing/prospecto/jaxrs/runtime/reader/XmlViewMessageBodyReader.java
@@ -0,0 +1,64 @@
+/*
+ * File created on Mar 27, 2016
+ *
+ * Copyright (c) 2016 Carl Harris, Jr
+ * and others as noted
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.soulwing.prospecto.jaxrs.runtime.reader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Collections;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.Provider;
+
+import org.soulwing.prospecto.ViewReaderFactoryProducer;
+import org.soulwing.prospecto.api.View;
+import org.soulwing.prospecto.api.ViewReaderFactory;
+
+/**
+ * A {@link MessageBodyReader} for XML.
+ *
+ * @author Carl Harris
+ */
+@Provider
+@Consumes({ MediaType.APPLICATION_XML, MediaType.TEXT_XML })
+public class XmlViewMessageBodyReader implements MessageBodyReader<View> {
+
+  private static final ViewReaderFactory readerFactory =
+      ViewReaderFactoryProducer.getFactory("XML",
+          Collections.<String, Object>emptyMap());
+
+  @Override
+  public boolean isReadable(Class<?> aClass, Type type, Annotation[] annotations,
+      MediaType mediaType) {
+    return true;
+  }
+
+  @Override
+  public View readFrom(Class<View> aClass, Type type, Annotation[] annotations,
+      MediaType mediaType, MultivaluedMap<String, String> multivaluedMap,
+      InputStream inputStream) throws IOException, WebApplicationException {
+    return readerFactory.newReader(inputStream).readView();
+  }
+
+}
